3aa2ed6ae47073b69aec8a3d88a3b74abbf4f2a0
Your Name <you@example.com>
2019-07-05 23:28:42 +0200

    CORS! :)

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const { createServer, plugins } = require("restify");
+const corsMiddleware = require('restify-cors-middleware')
 
 const server = createServer({
   name: "myapp",
@@ -8,6 +9,14 @@ const server = createServer({
 server.use(plugins.acceptParser(server.acceptable));
 server.use(plugins.queryParser());
 server.use(plugins.bodyParser());
+
+const cors = corsMiddleware({
+  preflightMaxAge: 5,
+  origins: ['http://netlify.com']
+})
+ 
+server.pre(cors.preflight);
+server.use(cors.actual);
 
 server.use((req, res, next) => {
   console.log(req.url, req.body);
