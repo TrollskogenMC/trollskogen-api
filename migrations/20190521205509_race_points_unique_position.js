621dfbf7ee46905d0ef4f92f5b2f75da147de2cd
Anthon Fredriksson <hornta@protonmail.com>
2019-11-23 23:30:36 +0100

    -

--- migrations/20190521205509_race_points_unique_position.js
+++ migrations/20190521205509_race_points_unique_position.js
@@ -1,5 +1,5 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.alterTable("race_points", function(t) {
+  return knex.schema.alterTable("race_points", (t) => {
     t.unique(["race_id", "position"]);
     t.unique(["race_id", "location_x", "location_y", "location_z"]);
   });
