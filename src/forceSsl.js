efb1ac4e0b989fbb65694e8c3f482881f95afc36
Anthon Fredriksson <hornta@protonmail.com>
2019-11-28 00:24:40 +0100

    lint fix

--- src/forceSsl.js
+++ src/forceSsl.js
@@ -1,6 +1,6 @@
 export default function(req, res, next) {
   if (process.env.NODE_ENV === "production") {
-    if (req.headers["x-forwarded-proto"] != "https") {
+    if (req.headers["x-forwarded-proto"] !== "https") {
       res.redirect(302, `https://${process.env.HOST}${req.url}`, next);
     } else {
       next();
